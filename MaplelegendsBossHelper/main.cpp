39caf71f6cafa80ff2a7d9ad5d3ebf7183e58e85
mquinaz <miguelquinaz96@gmail.com>
2025-07-16 23:54:07 +0100

    improvement of base UI

--- MaplelegendsBossHelper/main.cpp
+++ MaplelegendsBossHelper/main.cpp
@@ -8,8 +8,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    Timer t;
-
     w.show();
     return a.exec();
 }
--- MaplelegendsBossHelper/mainwindow.cpp
+++ MaplelegendsBossHelper/mainwindow.cpp
@@ -2,6 +2,11 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QTimer>
+#include <QWidget>
+#include <QScreen>
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
 #include "timer.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -10,7 +15,11 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    this->setFixedSize(QSize(750, 400));
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+    this->setFixedSize(QSize(width, height));
 
     QPixmap bkgnd(":/res/background.jpg");
     bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
@@ -18,7 +27,45 @@ MainWindow::MainWindow(QWidget *parent)
     palette.setBrush(QPalette::Window, bkgnd);
     this->setPalette(palette);
 
-    //Timer t;
+    QLabel *label4 = new QLabel(this);
+    label4->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    label4->setStyleSheet("border-image: url(:/res/Mano2.png) stretch;");
+    label4->setAlignment(Qt::AlignCenter);
+    label4->setGeometry(QRect(75,50,150,150));
+
+    QLabel *label6 = new QLabel(this);
+    label6->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    label6->setAlignment(Qt::AlignCenter);
+    label6->setText("-");
+    label6->setGeometry(QRect(125,25,50,25));
+
+    QLabel *label = new QLabel(this);
+    label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    label->setStyleSheet("border: 1px solid; border-color:rgba(212,225,229,122); ");
+    label->setText("-");
+    label->setAlignment(Qt::AlignCenter);
+    label->setGeometry(QRect(100,200,100,30));
+
+    QLabel *label2 = new QLabel(this);
+    label2->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    label2->setStyleSheet("border: 1px solid; border-color:rgba(212,225,229,122); ");
+    label2->setText("-10%");
+    label2->setAlignment(Qt::AlignCenter);
+    label2->setGeometry(QRect(50,200,50,30));
+
+    QLabel *label3 = new QLabel(this);
+    label3->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    label3->setStyleSheet("border: 1px solid; border-color:rgba(212,225,229,122); ");
+    label3->setText("+10%");
+    label3->setAlignment(Qt::AlignCenter);
+    label3->setGeometry(QRect(200,200,50,30));
+
+    QPushButton *button = new QPushButton(this);
+    button->setStyleSheet("border-image: url(:/res/dead-head.png) stretch;");
+    button->setGeometry(QRect(200,300,50,50));
+    QObject::connect(&button, &QPushButton::clicked, on_pushButton_clicked);
+
+    Timer t;
 }
 
 MainWindow::~MainWindow()
